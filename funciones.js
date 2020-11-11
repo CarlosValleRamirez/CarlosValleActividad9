e9d1626c1bc0c647755fd8a0ab1effc35240b9f3
CarlosValleRamirez <Carlos_Valle_Ramirez@hotmail.com>
2020-11-11 09:55:31 -0600

    Funciones  JavaScript

---
+++ funciones.js
@@ -0,0 +1,13 @@
+console.log ("Primera Practica");
+
+var variableVar = "Variable con Var" ;
+let variableLet = "Variable con Let";
+const variableConst = " Variable con Const";
+
+console.log(variableVar);
+console.log(variableLet);
+console.log(variableConst);
+
+var variableVar = "Variable con Var";
+
+variableConst = "Nuevo Valor de Variable Const";
